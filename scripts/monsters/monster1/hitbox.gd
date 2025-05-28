c8704c3785a0f3a42f2df4ff2d45de525eb8c780
Ahmad Ghozi Fidinillah <ahmadghozi1234@gmail.com>
2025-05-28 10:17:24 +0700

    add 1 second delay on claws hitbox after triggering damage

--- scripts/monsters/monster1/hitbox.gd
+++ scripts/monsters/monster1/hitbox.gd
@@ -1,1 +1,9 @@
 extends Hitbox
+
+
+func _on_hit_enemy() -> void:
+    get_tree().create_timer(1).timeout.connect(on_timer_timeout)
+    $CollisionShape2D.disabled = true
+
+func on_timer_timeout():
+    $CollisionShape2D.disabled = false
